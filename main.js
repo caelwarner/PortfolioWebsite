5901dd87ec50238d9be4f6a677f708e129a95be4
Cael Warner <52902343+CaelWarner@users.noreply.github.com>
2021-12-16 00:40:00 -0800

    Added complex multi layered noise
    - Noise now has octaves
    - Added SimpleNoiseFilter for basic multi layered noise
    - Added RidgeNoiseFilter for creating mountain ridges

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ class TerrainFace {
     #resolution;
     #vertexIdOffset;
     #localUp;
-    #noise3D;
+    #planet;
 
     #axisA;
     #axisB;
@@ -18,11 +18,11 @@ class TerrainFace {
     #vertices = [];
     #indices = [];
 
-    constructor(resolution, vertexIdOffset, localUp, noise3D) {
+    constructor(resolution, vertexIdOffset, localUp, planet) {
         this.#resolution = resolution;
         this.#vertexIdOffset = vertexIdOffset;
         this.#localUp = localUp;
-        this.#noise3D = noise3D;
+        this.#planet = planet;
 
         this.#axisA = new Vector3(localUp.y, localUp.z, localUp.x);
         this.#axisB = new Vector3().crossVectors(localUp, this.#axisA);
@@ -37,10 +37,7 @@ class TerrainFace {
                 let pointOnUnitCube = this.#localUp.clone().add(this.#axisA.clone().multiplyScalar((percent.x - 0.5) * 2)).add(this.#axisB.clone().multiplyScalar((percent.y - 0.5) * 2));
                 let pointOnUnitSphere = pointOnUnitCube.clone().normalize();
 
-                let strength = 0.3;
-                let roughness = 3.0;
-
-                pointOnUnitSphere.multiplyScalar((this.#noise3D(...pointOnUnitSphere.clone().multiplyScalar(roughness)) * strength) + 1);
+                pointOnUnitSphere.multiplyScalar(this.#planet.calculateElevation(pointOnUnitSphere));
 
                 this.#vertices.push(...pointOnUnitSphere);
 
@@ -75,7 +72,12 @@ class Planet {
 
     #resolution;
     #geometry = new THREE.BufferGeometry();
-    #noise3D = makeNoise3D(Date.now());
+
+    #noiseFilters = [
+        new SimpleNoiseFilter(0.075, 6, 1.2, 1.8, 0.525, 1.05, false, 3),
+        // new SimpleNoiseFilter(6, 5, 1.1, 2.5, 0.5, 1.2, true, 3 + 9342857),
+        new RidgeNoiseFilter(3, 5,2.6, 3, 0.5, 1.1, 1, true, 3 + 58439050)
+    ];
 
     constructor(resolution) {
         this.#resolution = resolution;
@@ -84,12 +86,30 @@ class Planet {
         this.createGeometry();
     }
 
+    calculateElevation(position) {
+        let firstLayerElevation = this.#noiseFilters[0].evaluate(position);
+        let elevation = firstLayerElevation;
+
+        for (let i = 1; i < this.#noiseFilters.length; i++) {
+            let mask = this.#noiseFilters[i].useFirstLayerAsMask ? firstLayerElevation : 1;
+
+            elevation += this.#noiseFilters[i].evaluate(position) * mask;
+        }
+
+        return elevation + 1;
+    }
+
     createVerticesAndIndices() {
         Planet.directions.forEach(direction => {
-            let terrainFace = new TerrainFace(this.#resolution, this.#vertices.length / 3, direction, this.#noise3D);
+            let terrainFace = new TerrainFace(this.#resolution, this.#vertices.length / 3, direction, this);
 
-            this.#vertices.push(...terrainFace.getVertices());
-            this.#indices.push(...terrainFace.getIndices());
+            for (let i = 0; i < terrainFace.getVertices().length; i++) {
+                this.#vertices.push(terrainFace.getVertices()[i]);
+            }
+
+            for (let i = 0; i < terrainFace.getIndices().length; i++) {
+                this.#indices.push(terrainFace.getIndices()[i]);
+            }
         });
     }
 
@@ -105,6 +125,82 @@ class Planet {
 
 }
 
+class SimpleNoiseFilter {
+
+    #evaluateNoise;
+
+    constructor(strength, numLayers, baseRoughness, roughness, persistence, minHeight, useFirstLayerAsMask, seed) {
+        this.strength = strength;
+        this.numLayers = numLayers;
+        this.baseRoughness = baseRoughness;
+        this.roughness = roughness;
+        this.persistence = persistence;
+        this.minHeight = minHeight;
+        this.useFirstLayerAsMask = useFirstLayerAsMask;
+        this.#evaluateNoise = makeNoise3D(seed);
+    }
+
+    evaluate(position) {
+        let elevation = 0;
+        let frequency = this.baseRoughness;
+        let amplitude = 1;
+
+        for (let i = 0; i < this.numLayers; i++) {
+            let nextLayerNoise = this.#evaluateNoise(...position.clone().multiplyScalar(frequency));
+            elevation += (nextLayerNoise + 1) * 0.5 * amplitude;
+
+            frequency *= this.roughness;
+            amplitude *= this.persistence;
+        }
+
+        elevation = Math.max(0, elevation - this.minHeight);
+
+        return elevation * this.strength;
+    }
+
+}
+
+class RidgeNoiseFilter {
+
+    #evaluateNoise;
+
+    constructor(strength, numLayers, baseRoughness, roughness, persistence, minHeight, weightMultiplier, useFirstLayerAsMask, seed) {
+        this.strength = strength;
+        this.numLayers = numLayers;
+        this.baseRoughness = baseRoughness;
+        this.roughness = roughness;
+        this.persistence = persistence;
+        this.minHeight = minHeight;
+        this.weightMultiplier = weightMultiplier;
+        this.useFirstLayerAsMask = useFirstLayerAsMask;
+        this.#evaluateNoise = makeNoise3D(seed);
+    }
+
+    evaluate(position) {
+        let elevation = 0;
+        let frequency = this.baseRoughness;
+        let amplitude = 1;
+        let weight = 1;
+
+        for (let i = 0; i < this.numLayers; i++) {
+            let nextLayerNoise = 1 - Math.abs(this.#evaluateNoise(...position.clone().multiplyScalar(frequency)));
+            nextLayerNoise *= nextLayerNoise;
+            nextLayerNoise *= weight;
+            weight = Math.min(Math.max(nextLayerNoise * this.weightMultiplier, 0), 1);
+
+            elevation += (nextLayerNoise + 1) * 0.5 * amplitude;
+
+            frequency *= this.roughness;
+            amplitude *= this.persistence;
+        }
+
+        elevation = Math.max(0, elevation - this.minHeight);
+
+        return elevation * this.strength;
+    }
+
+}
+
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer({
@@ -113,14 +209,11 @@ const renderer = new THREE.WebGLRenderer({
 
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.outputEncoding = THREE.sRGBEncoding;
 camera.position.setZ(30);
 
-const planet = new Planet(100);
+const planet = new Planet(256);
 
-console.log(planet.getGeometry());
-
-const material = new THREE.MeshStandardMaterial({ color: 0x3300FF });
+const material = new THREE.MeshStandardMaterial({ color: 0xAAAAAA });
 const object = new THREE.Mesh(planet.getGeometry(), material);
 
 const light = new THREE.HemisphereLight(0xFFFFFF, 0x000000);
