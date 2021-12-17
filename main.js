54ccfa2a8ffd3f883b99ddd836b78f87a15fc5d1
Cael Warner <52902343+CaelWarner@users.noreply.github.com>
2021-12-16 23:22:25 -0800

    Added colours 🌈
    - Added gradient colour system that assigns colour based on elevation

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 import "./style.css";
 
 import * as THREE from "three";
-import { Vector2, Vector3 } from "three";
+import {MathUtils, Vector2, Vector3} from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { makeNoise3D } from "open-simplex-noise";
+import {MathUtil} from "three/examples/jsm/libs/OimoPhysics";
 
 class TerrainFace {
 
@@ -15,8 +16,11 @@ class TerrainFace {
     #axisA;
     #axisB;
 
+    #elevations = [];
+
     #vertices = [];
     #indices = [];
+    #colors = [];
 
     constructor(resolution, vertexIdOffset, localUp, planet) {
         this.#resolution = resolution;
@@ -37,8 +41,10 @@ class TerrainFace {
                 let pointOnUnitCube = this.#localUp.clone().add(this.#axisA.clone().multiplyScalar((percent.x - 0.5) * 2)).add(this.#axisB.clone().multiplyScalar((percent.y - 0.5) * 2));
                 let pointOnUnitSphere = pointOnUnitCube.clone().normalize();
 
-                pointOnUnitSphere.multiplyScalar(this.#planet.calculateElevation(pointOnUnitSphere));
+                let elevation = this.#planet.calculateElevation(pointOnUnitSphere);
+                pointOnUnitSphere.multiplyScalar(elevation);
 
+                this.#elevations.push(elevation);
                 this.#vertices.push(...pointOnUnitSphere);
 
                 if (x !== this.#resolution - 1 && y !== this.#resolution - 1) {
@@ -61,6 +67,10 @@ class TerrainFace {
     getIndices() {
         return this.#indices;
     }
+
+    getElevations() {
+        return this.#elevations;
+    }
 }
 
 class Planet {
@@ -69,15 +79,24 @@ class Planet {
 
     #vertices = [];
     #indices = [];
+    #colors = [];
 
     #resolution;
     #geometry = new THREE.BufferGeometry();
 
+    #minElevation = Number.MAX_VALUE;
+    #maxElevation = Number.MIN_VALUE;
+
+    #colorCache = new Map();
+
     #noiseFilters = [
-        new SimpleNoiseFilter(0.075, 6, 1.2, 1.8, 0.525, 1.05, false, 3),
+        new SimpleNoiseFilter(0.075, 6, 1.2, 1.8, 0.525, 1.05, false, Date.now()),
         // new SimpleNoiseFilter(6, 5, 1.1, 2.5, 0.5, 1.2, true, 3 + 9342857),
-        new RidgeNoiseFilter(3, 5,2.6, 3, 0.5, 1.1, 1, true, 3 + 58439050)
+        new RidgeNoiseFilter(3, 5,2.6, 3, 0.5, 1.1, 1, true, Date.now() + 58439050)
     ];
+
+    // #colorScheme = new ColorGenerator([new Vector3(0, 105, 148), new Vector3(86, 125, 70), new Vector3(57, 140, 10), new Vector3(70, 46, 26), new Vector3(255, 250, 250)], [0, 0.05, 0.5, 0.7, 1]);
+    #colorScheme = new ColorGenerator([new Vector3(116, 93, 235), new Vector3(210, 221, 56), new Vector3(203, 46, 30), new Vector3(203, 46, 30)], [0, 0.1, 0.6, 1]);
 
     constructor(resolution) {
         this.#resolution = resolution;
@@ -96,10 +115,28 @@ class Planet {
             elevation += this.#noiseFilters[i].evaluate(position) * mask;
         }
 
-        return elevation + 1;
+        elevation += 1;
+        this.addMinMaxElevation(elevation);
+
+        return elevation;
+    }
+
+    calculateColor(elevation) {
+        if (this.#colorCache.has(elevation)) {
+            return this.#colorCache.get(elevation);
+        }
+
+        let color = this.#colorScheme.evaluate(elevation, this.#minElevation, this.#maxElevation).clone();
+        color.divideScalar(255);
+
+        this.#colorCache.set(elevation, color);
+
+        return color;
     }
 
     createVerticesAndIndices() {
+        let elevations = [];
+
         Planet.directions.forEach(direction => {
             let terrainFace = new TerrainFace(this.#resolution, this.#vertices.length / 3, direction, this);
 
@@ -110,17 +147,36 @@ class Planet {
             for (let i = 0; i < terrainFace.getIndices().length; i++) {
                 this.#indices.push(terrainFace.getIndices()[i]);
             }
+
+            for (let i = 0; i < terrainFace.getElevations().length; i++) {
+                elevations.push(terrainFace.getElevations()[i]);
+            }
         });
+
+        elevations.forEach(elevation => {
+            this.#colors.push(...this.calculateColor(elevation));
+        })
     }
 
     createGeometry() {
         this.#geometry.setIndex(this.#indices);
         this.#geometry.setAttribute("position", new THREE.Float32BufferAttribute(this.#vertices, 3));
+        this.#geometry.setAttribute("color", new THREE.Float32BufferAttribute(this.#colors, 3));
         this.#geometry.computeVertexNormals();
     }
 
     getGeometry() {
         return this.#geometry;
+    }
+
+    addMinMaxElevation(value) {
+        if (value > this.#maxElevation) {
+            this.#maxElevation = value;
+        }
+
+        if (value < this.#minElevation) {
+            this.#minElevation = value;
+        }
     }
 
 }
@@ -186,7 +242,7 @@ class RidgeNoiseFilter {
             let nextLayerNoise = 1 - Math.abs(this.#evaluateNoise(...position.clone().multiplyScalar(frequency)));
             nextLayerNoise *= nextLayerNoise;
             nextLayerNoise *= weight;
-            weight = Math.min(Math.max(nextLayerNoise * this.weightMultiplier, 0), 1);
+            weight = MathUtils.clamp(nextLayerNoise * this.weightMultiplier, 0, 1);
 
             elevation += (nextLayerNoise + 1) * 0.5 * amplitude;
 
@@ -197,6 +253,29 @@ class RidgeNoiseFilter {
         elevation = Math.max(0, elevation - this.minHeight);
 
         return elevation * this.strength;
+    }
+
+}
+
+class ColorGenerator {
+
+    constructor(colors, positions) {
+        this.colors = colors;
+        this.positions = positions;
+    }
+
+    evaluate(elevation, min, max) {
+        let position = MathUtils.inverseLerp(min, max, elevation);
+
+        for (let i = 0; i < this.positions.length; i++) {
+            if (this.positions[i] > position) {
+                let localPosition = MathUtils.inverseLerp(this.positions[i - 1], this.positions[i], position);
+
+                return this.colors[i - 1].clone().lerp(this.colors[i], localPosition);
+            }
+        }
+
+        return this.colors[this.colors.length - 1];
     }
 
 }
@@ -213,7 +292,7 @@ camera.position.setZ(30);
 
 const planet = new Planet(256);
 
-const material = new THREE.MeshStandardMaterial({ color: 0xAAAAAA });
+const material = new THREE.MeshPhongMaterial({ vertexColors: true });
 const object = new THREE.Mesh(planet.getGeometry(), material);
 
 const light = new THREE.HemisphereLight(0xFFFFFF, 0x000000);
